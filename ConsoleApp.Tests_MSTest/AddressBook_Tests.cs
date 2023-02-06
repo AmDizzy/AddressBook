a488eb7fe44e139f5ce1893ae414f282e6e736b4
AmDizzy <abdojoh@hotmail.com>
2023-02-06 16:49:45 +0100

    Added MSTest

---
+++ ConsoleApp.Tests_MSTest/AddressBook_Tests.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp.Tests_MSTest;
+using ConsoleApp.Models;
+
+[TestClass]
+public class Addressbook_Tests
+{
+    [TestMethod]
+    public void Should_Add_Contact_To_List()
+    {
+        //Arrange
+        Contact contact = new();
+
+        //Act
+
+        //Assert
+    }
+}
--- ConsoleApp/Models/Contact.cs
+++ ConsoleApp/Models/Contact.cs
@@ -2,7 +2,7 @@
 
 namespace ConsoleApp.Models;
 
-internal class Contact : IContact
+public class Contact : IContact
 {
     public Guid Id { get; set; } = Guid.NewGuid();
     public string FirstName { get; set; } = null!;
